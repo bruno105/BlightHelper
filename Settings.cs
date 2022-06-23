4d5430b3a6ed1354f59abae3671dfee4a4ea75cc
bruno105 <bruno1057_@hotmail.com>
2022-06-22 21:34:17 -0300

    Create

---
+++ Settings.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows.Forms;
+using ExileCore.Shared.Attributes;
+using ExileCore.Shared.Interfaces;
+using ExileCore.Shared.Nodes;
+
+namespace BlightHelper
+{
+    public class Settings : ISettings
+    {
+
+
+        public ToggleNode Enable { get; set; } = new ToggleNode(false);
+
+        public ToggleNode Debug2 { get; set; } = new ToggleNode(false);
+
+        public ButtonNode RefreshFile { get; set; } = new ButtonNode();        
+        public RangeNode<int> FrameThickness { get; set; } = new RangeNode<int>(2, 2, 5);
+        public ColorNode BaseColor { get; set; } = new ColorNode(SharpDX.Color.DeepPink);
+
+
+
+
+
+    }
+}
